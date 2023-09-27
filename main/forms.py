55ae916da5178bf292c2ad7d92392286d08b761b
Akmal76 <akmalramadhan76@gmail.com>
2023-09-27 14:13:05 +0700

    Mengerjakan tutorial 4

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,13 @@
 from django.forms import ModelForm
 from main.models import Product
-from django import forms
-from django.contrib.auth.models import User
+# from django import forms
+# from django.contrib.auth.models import User
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "price", "description", "user"]
+        fields = ["name", "price", "description",]
 
     # Baris baru ini menimpa atau 'override' field user yang otomatis ada dari ModelForm.
     # Gunanya untuk menambahkan atribut tertentu, seperti required=True pada contoh
-    user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)  # NEW 
+    # user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)  # NEW 
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
 app_name = 'main'
 
 urlpatterns = [
@@ -12,4 +12,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
 ]
